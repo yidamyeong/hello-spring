1f05736f173338e0e8732caa13285590fdfb30f4
2dam0 <yidamyeong@gmail.com>
2021-06-14 02:19:12 +0900

    initial commit

---
+++ src/main/java/com/dam/hellospring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.dam.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hi, i am so called data");
+        return "hello";
+    }
+
+    @GetMapping("login")
+    public String login(Model model) {
+        model.addAttribute("id", "jess");
+        model.addAttribute("nickname", "dam0");
+        return "login";
+    }
+}
