485703acfd51f67561733e24e4ea638e9f22d1e8
DESKTOP-4ISDJ05\2dam0 <yidamyeong@wehago.com>
2021-06-18 18:08:00 +0900

    RabbitConfig 수정

--- src/main/java/com/dam/hellospring/configuration/RabbitConfig.java
+++ src/main/java/com/dam/hellospring/configuration/RabbitConfig.java
@@ -1,4 +1,45 @@
 package com.dam.hellospring.configuration;
 
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitConfig {
+
+    private static final String queueName = "spring-boot";
+    private static final String topicExchangeName = "spring-boot-exchange";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("rabbit");
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate((org.springframework.amqp.rabbit.connection.ConnectionFactory) connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public org.springframework.amqp.support.converter.MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
